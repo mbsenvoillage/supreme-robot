2f8d6a8b9bfb418b2f3fabdfcc609fc6cc9ab978
WikiFamillemaintainer <122366189+WikiFamillemaintainer@users.noreply.github.com>
2023-02-08 10:09:21 +0100

    xool

---
+++ JS/challengeulysse/code.js
@@ -0,0 +1,66 @@
+function name (){
+    let name1 =prompt("Quel est ton nom")
+    alert(name1+' '+'est ton nom')
+
+}
+name()
+Math.random()
+function math(){
+
+    Math()
+    const v1=Math.random
+    let a = p=prompt("4+4");
+
+    if (a=4) {
+        alert("Bonne réponse!!");
+    } else {
+        alert("La réponse est "+4); 
+    }
+
+    let b = prompt ("Q2 : 3 + 3 = ?");
+    if (b==6) {
+        alert("Bonne réponse!!");
+    } else {
+        alert("La réponse est 6!!");
+    }
+
+    let c = prompt ("Q2 : 4 + 3 = ?");
+    if (b==7) {
+        alert("Bonne réponse!!");
+    } else {
+        alert("La réponse est 7!!");
+    }
+
+    let d = prompt ("Q2 : 0 + 3 = ?");
+    if (b==3) {
+        alert("Bonne réponse!!");
+    } else {
+        alert("La réponse est 3!!");
+    }
+
+    let results =4;
+
+  if(a!=4){
+results=results-1 
+
+
+  }
+  if(b!=6){
+    results=results-1
+
+  };
+  if(c!=7)
+{
+results=results-1
+
+}
+if (d!=3){
+results=results-1
+
+}
+alert(" ta note est de " + results+ "/4")
+function convertisseur (){
+    
+}
+};
+
