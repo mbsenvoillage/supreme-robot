749a645a6baa0c758b3bc72fa95916b4d6b53e12
Yunanistan24 <121807846+Yunanistan24@users.noreply.github.com>
2023-02-14 13:37:38 +0100

    cool

---
+++ JS/challengeyuna/code.js
@@ -0,0 +1,11 @@
+function convertisseur (){
+    let value=prompt("Entrez la note à convertir")
+    if(value=>18){
+        alert("A+ excellent ")
+    }
+    if(value>=15&&value>=17){
+        alert("A bravo ")
+    }
+    if(value>=13&&value<=14){
+        alert("B")
+    }
