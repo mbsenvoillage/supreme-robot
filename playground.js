5e9f540bb0a40ca4de7d0e7f495fbce06cd55b54
YvvY <momboisse.yvn@gmail.com>
2023-03-29 19:53:32 +0200

    more playground stuff

--- playground.js
+++ playground.js
@@ -57,17 +57,90 @@
 // console.log(res);
 //boucle qui calcule le résultat d
 
-let b = ["a", "b", "c"];
-console.log(b.length);
+// let b = ["a", "b", "c"];
+// console.log(b.length);
 
-console.log(b[0]);
-for (let i = 0; i < b.length; i++) {
-  console.log(b[i]);
+// console.log(b[0]);
+// for (let i = 0; i < b.length; i++) {
+//   console.log(b[i]);
+// }
+
+// let u = "";
+// for (let i = 0; i < b.length; i++) {
+//   u += b[i];
+// }
+
+// console.log(u);
+
+//let b = [35, 36, 37];
+
+//onsole.log(b);
+//console.log(b[0]);
+//b[0] = "Yuna";
+
+//console.log(b);
+
+// for (let o = 0; o < b.length; o++) {
+//   console.log(b[o]);
+// }
+
+let kitten = {
+  name: "kitty",
+  colour: "brown",
+  skills: "eat, jump, run ",
+  age: "3",
+  length: "30cm",
+  food: "salmon",
+  home: "4 bvd Cateunage, Princesse",
+  eat: (nourriture) => {
+    console.log("Je mange des " + nourriture);
+  },
+  makeNoise: (noise) => {
+    console.log(noise);
+  },
+  calc: (a, b) => {
+    return a + b;
+  },
+};
+console.log(kitten);
+console.log(kitten.name);
+
+kitten.eat("croquettes");
+kitten.makeNoise("Miaou");
+console.log(kitten.calc(2, 2));
+kitten.father = {
+  name: "John",
+  colour: "Blue",
+};
+
+console.log(kitten);
+
+let user1 = {
+  username: "Billy",
+  password: "1234",
+  banned: false,
+};
+
+let user2 = {
+  username: "Jimmy",
+  password: "IUYIUYDIUYgfihzfoihsdfoihsdger!é§",
+  banned: false,
+};
+
+let users = [user1, user2];
+
+for (let i = 0; i < users.length; i++) {
+  users[i].banned = true;
 }
 
-let u = "";
-for (let i = 0; i < b.length; i++) {
-  u += b[i];
-}
+console.log(users);
 
-console.log(u);
+let a = ["z", "e"];
+
+a[0] = "erueruyer";
+
+console.log(a);
+
+user2.banned = false;
+
+console.log(user2);
